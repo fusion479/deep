ce23ef436d4bf832eb96dc498e20c6cb6d8d4ddf
Bi1ku <2008owenshi@gmail.com>
2024-06-28 09:40:35 -0400

    Trajectories base template

--- MeepMeep/src/main/java/com/example/meepmeep/Trajectories.java
+++ MeepMeep/src/main/java/com/example/meepmeep/Trajectories.java
@@ -1,4 +1,25 @@
 package com.example.meepmeep;
 
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+
 public class Trajectories {
+    private final Color color;
+
+    public Trajectories(Color color) {
+        this.color = color;
+    }
+
+    public Pose2d reflectY(Pose2d pose) {
+        return this.color == Color.RED ? new Pose2d(pose.position.x, -pose.position.y, Math.toRadians(360) - pose.heading.toDouble()) : pose;
+    }
+
+    public Vector2d reflectY(Vector2d vector) {
+        return this.color == Color.RED ? new Vector2d(vector.x, -vector.y) : vector;
+    }
+
+    public enum Color {
+        BLUE,
+        RED
+    }
 }
