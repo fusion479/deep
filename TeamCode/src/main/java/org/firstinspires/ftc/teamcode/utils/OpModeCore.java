c0db7df3900faf651c58212f564c105d3baea07c
Bi1ku <2008owenshi@gmail.com>
2024-07-04 14:21:39 -0400

    bulk read & cahche senors and servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/OpModeCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/OpModeCore.java
@@ -4,9 +4,13 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import java.util.List;
 
@@ -15,9 +19,11 @@ public abstract class OpModeCore extends CommandOpMode {
     private final ElapsedTime period = new ElapsedTime();
     private final ElapsedTime startUp = new ElapsedTime();
 
-    // INITIALIZE HUBS AND MOTORS
+    // INITIALIZE HUBS FOR BULK READING
     private final List<LynxModule> hubs = super.hardwareMap.getAll(LynxModule.class);
     private final List<DcMotorEx> motors = super.hardwareMap.getAll(DcMotorEx.class);
+    private final List<ServoEx> servos = super.hardwareMap.getAll(ServoEx.class);
+    private final List<ColorRangeSensor> sensors = super.hardwareMap.getAll(ColorRangeSensor.class);
 
     public void logCycles() {
         this.multipleTelemetry.addData("Period (Seconds / 1 Cycle): ", this.period.seconds());
@@ -38,6 +44,15 @@ public abstract class OpModeCore extends CommandOpMode {
         for (DcMotorEx motor : this.motors) {
             motor.getCurrentPosition();
             motor.getVelocity();
+        }
+
+        for (ServoEx servo : this.servos) {
+            servo.getPosition();
+            servo.getAngle();
+        }
+
+        for (ColorRangeSensor sensor : this.sensors) {
+            sensor.getDistance(DistanceUnit.MM);
         }
     }
 
