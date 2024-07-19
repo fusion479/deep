aa6432f4b6ff84394ae394a47abf0cf1d4322d25
Bi1ku <2008owenshi@gmail.com>
2024-07-19 10:12:43 -0400

    minor updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandRobot.java
@@ -9,25 +9,31 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
 import org.firstinspires.ftc.teamcode.utils.commands.RobotCore;
 
 public class CommandRobot extends RobotCore {
     private final Drivetrain drivetrain;
+    private final Lift lift;
     private GamepadEx gamepad1;
     private GamepadEx gamepad2;
 
+    // TELEOP
     public CommandRobot(Type type, HardwareMap hwMap, MultipleTelemetry telemetry, Gamepad gamepad1, Gamepad gamepad2) {
-        super(type, telemetry);
+        super(type, telemetry); // TODO: get rid of type and check for gamepad?
 
         this.drivetrain = new Drivetrain(hwMap, telemetry, new Pose2d(0, 0, 0));
+        this.lift = new Lift(hwMap, telemetry);
         this.gamepad1 = new GamepadEx(gamepad1);
         this.gamepad2 = new GamepadEx(gamepad2);
     }
 
+    // AUTON
     public CommandRobot(Type type, HardwareMap hwMap, Pose2d startPose, MultipleTelemetry telemetry) {
         super(type, telemetry);
 
         this.drivetrain = new Drivetrain(hwMap, telemetry, startPose);
+        this.lift = new Lift(hwMap, telemetry);
     }
 
     public MecanumDrive getDrive() {
@@ -45,5 +51,6 @@ public class CommandRobot extends RobotCore {
     @Override
     public void startThreads(CommandOpMode opMode) {
         this.drivetrain.asyncDrive(this.gamepad1, opMode);
+        this.lift.asyncPower(opMode);
     }
 }
