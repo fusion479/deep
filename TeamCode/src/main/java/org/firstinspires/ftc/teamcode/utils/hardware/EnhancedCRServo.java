e2349dd66b3f420cd825ffaff2fcbbd24bfb5c72
Bi1ku <2008owenshi@gmail.com>
2024-09-28 11:11:22 -0400

    enhanced cr servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/hardware/EnhancedCRServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/hardware/EnhancedCRServo.java
@@ -8,14 +8,20 @@ public class EnhancedCRServo {
     private final CRServo servo;
     private final PIDController controller;
 
+    private double position;
+
     public EnhancedCRServo(CRServo servo) {
         this.servo = servo;
 
         this.controller = new PIDController(0, 0, 0, 0);
     }
 
+    public void update() {
+        this.servo.setPower(this.controller.calculate(this.position));
+    }
+
     public void setPosition(double position) {
-        this.servo.setPower(this.controller.calculate(position));
+        this.position = position;
     }
 
     public void setPower(double power) {
