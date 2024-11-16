42c3c2fd74de0b2ad2027dcfad04025846f89f77
Bi1ku <2008owenshi@gmail.com>
2024-11-16 11:04:55 -0500

    faster deacceleration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -14,8 +14,12 @@ import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
 @Config
 public class Drivetrain extends SubsystemBase {
-    public static double MAX_ACCEL = 0.15;
-    public static double MAX_ANGULAR_ACCEL = 0.15;
+    public static double MAX_ACCEL = 0.1;
+    public static double MAX_ANGULAR_ACCEL = 0.1;
+
+    public static double MAX_DEACCEL = 0.5;
+    public static double MAX_ANGULAR_DEACCEL = 0.5;
+
     public static double MAX_VEL = 0.75;
     public static double MAX_ANGULAR_VEL = 0.75;
 
@@ -32,40 +36,37 @@ public class Drivetrain extends SubsystemBase {
         this.drive = new MecanumDrive(hwMap, startPose);
     }
 
-    private static double relativeMin(double min, double check) {
-        double rel = Math.min(min, Math.abs(check));
+    private static double calculateAccel(double accel, double deaccel, double prevPower, double check) {
+        double rel;
 
-        return check >= 0 ? rel : -rel;
+        if (Math.abs(prevPower) > Math.abs(check))
+            rel = Math.min(deaccel, Math.abs(check - prevPower));
+        else rel = Math.min(accel, Math.abs(check - prevPower));
+
+        return check - prevPower >= 0 ? rel : -rel;
     }
 
     public void startThread(final GamepadEx gamepad, CommandOpMode opMode) {
         new Thread(() -> {
             while (opMode.opModeIsActive())
                 try {
-                    // TODO: Check if this works
                     synchronized (this.drive) {
-                        this.yPower += Drivetrain.relativeMin(MAX_ACCEL, gamepad.getLeftY() - this.yPower);
-                        this.xPower += Drivetrain.relativeMin(MAX_ACCEL, gamepad.getLeftX() - this.xPower);
-                        this.angPower += Drivetrain.relativeMin(MAX_ANGULAR_ACCEL, gamepad.getRightX() - this.angPower);
+                        this.yPower += Drivetrain.calculateAccel(MAX_ACCEL, MAX_DEACCEL, this.yPower, gamepad.getLeftY());
+                        this.xPower += Drivetrain.calculateAccel(MAX_ACCEL, MAX_DEACCEL, this.xPower, gamepad.getLeftX());
+                        this.angPower += Drivetrain.calculateAccel(MAX_ANGULAR_ACCEL, MAX_ANGULAR_DEACCEL, this.angPower, gamepad.getRightX());
 
                         this.drive.setDrivePowers(new PoseVelocity2d(
                                 new Vector2d(
                                         this.yPower * MAX_VEL,
-                                        this.xPower * MAX_VEL
+                                        -this.xPower * MAX_VEL
                                 ),
-                                this.angPower * MAX_ANGULAR_VEL));
+                                -this.angPower * MAX_ANGULAR_VEL));
                     }
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
         }).start();
-    }
-
-    @Override
-    public void periodic() {
-        this.telemetry.addData("xPower: ", this.xPower);
-        this.telemetry.addData("yPower: ", this.yPower);
     }
 
     public MecanumDrive getDrive() {
