b718319b52fc732ca45b63c4880abfe5051b4b8e
Stephen Chen <stephen.chen420@gmail.com>
2024-12-04 18:11:21 -0500

    new auton paths for far

--- PathVisualizer/src/main/java/com/example/meepmeep/PathVisualizer.java
+++ PathVisualizer/src/main/java/com/example/meepmeep/PathVisualizer.java
@@ -2,8 +2,10 @@ package com.example.meepmeep;
 
 import com.example.meepmeep.trajectories.BlueCloseBasket;
 import com.example.meepmeep.trajectories.BlueFarBasket;
+import com.example.meepmeep.trajectories.BlueFarPush;
 import com.example.meepmeep.trajectories.RedCloseBasket;
 import com.example.meepmeep.trajectories.RedFarBasket;
+import com.example.meepmeep.trajectories.RedFarPush;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -19,8 +21,10 @@ public class PathVisualizer {
 
         // BlueCloseBasket trajectories = new BlueCloseBasket(0);
         // BlueFarBasket trajectories = new BlueFarBasket(0);
+        // BlueFarPush trajectories = new BlueFarPush(0);
         // RedCloseBasket trajectories = new RedCloseBasket(0);
-         RedFarBasket trajectories = new RedFarBasket(0);
+        // RedFarBasket trajectories = new RedFarBasket(0);
+         RedFarPush trajectories = new RedFarPush(0);
 
         myBot.runAction(trajectories.start(myBot.getDrive().actionBuilder(trajectories.getStart())));
 
