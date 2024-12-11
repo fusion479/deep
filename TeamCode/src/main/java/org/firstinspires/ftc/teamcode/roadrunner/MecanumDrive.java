bce92a6ec4f638bf304bf66df1dc83c69b02bedd
Stephen Chen <stephen.chen420@gmail.com>
2024-12-11 17:29:07 -0500

    manualfeedforward gonna make me lose my mind

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -78,7 +78,7 @@ public class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 1.4023140718295783;
         public double kV = 0.12807128175751004;
-        public double kA = 0.1;
+        public double kA = 0.065;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
@@ -90,9 +90,9 @@ public class MecanumDrive {
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 1.0;
+        public double lateralGain = 1.0;
+        public double headingGain = 1.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
