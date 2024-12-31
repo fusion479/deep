5470141223b5308753439174843cc8da3d29ca01
Stephen Chen <stephen.chen420@gmail.com>
2024-12-30 23:03:01 -0500

    copy quickstart is a go

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/blue/CloseBasket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/blue/CloseBasket.java
@@ -2,34 +2,212 @@ package org.firstinspires.ftc.teamcode.opmodes.auton.blue;
 
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.CommandRobot;
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose;
-import org.firstinspires.ftc.teamcode.utils.commands.OpModeCore;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.Timer;
+
 
 @Autonomous(name = "Blue Close Basket", preselectTeleOp = "Main")
-public class CloseBasket extends OpModeCore {
+public class CloseBasket extends OpMode {
     private Follower follower;
     private CommandRobot robot;
 
+    private Timer pathTimer, actionTimer, opmodeTimer;
+
+    private int pathState;
+
+    private final Pose startPose = new Pose(8, 113, Math.toRadians(0));
+
+    private final Pose scorePose = new Pose(14, 129, Math.toRadians(315));
+
+    /** Lowest (First) Sample from the Spike Mark */
+    private final Pose pickup1Pose = new Pose(37, 121, Math.toRadians(0));
+
+    /** Middle (Second) Sample from the Spike Mark */
+    private final Pose pickup2Pose = new Pose(37, 132, Math.toRadians(0));
+
+    /** Highest (Third) Sample from the Spike Mark */
+    private final Pose pickup3Pose = new Pose(45.7, 133.2, Math.toRadians(90));
+
+    /** Park Pose for our robot, after we do all of the scoring. */
+    private final Pose parkPose = new Pose(60, 98, Math.toRadians(90));
+
+    /** Park Control Pose for our robot, this is used to manipulate the bezier curve that we will create for the parking.
+     * The Robot will not go to this pose, it is used a control point for our bezier curve. */
+    private final Pose parkControlPose = new Pose(65, 98, Math.toRadians(90));
+
+    private Path scorePreload, park;
+    private PathChain grabSpikemark1, grabSpikemark2, grabSpikemark3, scoreSpikemark1, scoreSpikemark2, scoreSpikemark3;
+
+    /** This method is called once at the init of the OpMode. **/
     @Override
-    public void initialize() {
-        // TODO: Input correct starting position
-        this.robot = new CommandRobot(super.hardwareMap, new Pose(0, 0, 0), super.multipleTelemetry, this);
+    public void init() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+        buildPaths();
     }
 
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
     @Override
-    public void runOpMode() {
-        CommandScheduler.getInstance().enable();
-        this.initialize();
+    public void loop() {
 
-        super.waitForStart();
+        // These loop the movements of the robot
+        follower.update();
+        autonomousPathUpdate();
 
-        // TODO: Add auto pathing and functionality
-        // TODO: Make an end command for roadrunner (see kookybotz)?
-        // TODO: Research how to stop Action.runBlocking() without error
+        // Feedback to Driver Hub
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
 
-        super.end();
+    public void buildPaths(){
+        scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scorePose)));
+        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
+
+        grabSpikemark1 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(scorePose), new Point(pickup1Pose)))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading())
+                .build();
+
+        scoreSpikemark1 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(pickup1Pose), new Point(scorePose)))
+                .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose.getHeading())
+                .build();
+
+        grabSpikemark2 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(scorePose), new Point(pickup2Pose)))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup2Pose.getHeading())
+                .build();
+
+        scoreSpikemark2 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(pickup2Pose), new Point(scorePose)))
+                .setLinearHeadingInterpolation(pickup2Pose.getHeading(), scorePose.getHeading())
+                .build();
+
+        grabSpikemark3 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(scorePose), new Point(pickup3Pose)))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup3Pose.getHeading())
+                .build();
+
+        scoreSpikemark3 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(pickup3Pose), new Point(scorePose)))
+                .setLinearHeadingInterpolation(pickup3Pose.getHeading(), scorePose.getHeading())
+                .build();
+
+        /* This is our park path. We are using a BezierCurve with 3 points, which is a curved line that is curved based off of the control point */
+        park = new Path(new BezierCurve(new Point(scorePose), /* Control Point */ new Point(parkControlPose), new Point(parkPose)));
+        park.setLinearHeadingInterpolation(scorePose.getHeading(), parkPose.getHeading());
+    }
+
+//- Follower State: "if(!follower.isBusy() {}" (Though, I don't recommend this because it might not return due to holdEnd
+//- Time: "if(pathTimer.getElapsedTimeSeconds() > 1) {}"
+//- Robot Position: "if(follower.getPose().getX() > 36) {}"
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.followPath(scorePreload);
+                setPathState(1);
+                break;
+            case 1:
+                if (follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    /* Score Preload */
+
+                    follower.followPath(grabSpikemark1, true);
+                    setPathState(2);
+                }
+                break;
+            case 2:
+                if (follower.getPose().getX() > (pickup1Pose.getX() - 1) && follower.getPose().getY() > (pickup1Pose.getY() - 1)) {
+                    /* Grab Sample */
+
+                    follower.followPath(scoreSpikemark1, true);
+                    setPathState(3);
+                }
+                break;
+            case 3:
+                if (follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    /* Score Sample */
+
+                    follower.followPath(grabSpikemark2, true);
+                    setPathState(4);
+                }
+                break;
+            case 4:
+                if (follower.getPose().getX() > (pickup2Pose.getX() - 1) && follower.getPose().getY() > (pickup2Pose.getY() - 1)) {
+                    /* Grab Sample */
+
+                    follower.followPath(scoreSpikemark2, true);
+                    setPathState(5);
+                }
+                break;
+            case 5:
+                if (follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    /* Score Sample */
+
+                    follower.followPath(grabSpikemark3, true);
+                    setPathState(6);
+                }
+                break;
+            case 6:
+                if (follower.getPose().getX() > (pickup3Pose.getX() - 1) && follower.getPose().getY() > (pickup3Pose.getY() - 1)) {
+                    /* Grab Sample */
+
+                    follower.followPath(scoreSpikemark3, true);
+                    setPathState(7);
+                }
+                break;
+            case 7:
+                if (follower.getPose().getX() > (scorePose.getX() - 1) && follower.getPose().getY() > (scorePose.getY() - 1)) {
+                    /* Score Sample */
+
+                    follower.followPath(park, true);
+                    setPathState(8);
+                }
+                break;
+            case 8:
+                if (follower.getPose().getX() > (parkPose.getX() - 1) && follower.getPose().getY() > (parkPose.getY() - 1)) {
+                    /* Level 1 Ascent */
+
+                    setPathState(-1);
+                }
+                break;
+        }
+    }
+
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+        /** This method is called continuously after Init while waiting for "play". **/
+    @Override
+    public void init_loop() {}
+
+    /** This method is called once at the start of the OpMode.
+     * It runs all the setup actions, including building paths and starting the path system **/
+    @Override
+    public void start() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    /** We do not use this because everything should automatically disable **/
+    @Override
+    public void stop() {
     }
 }
