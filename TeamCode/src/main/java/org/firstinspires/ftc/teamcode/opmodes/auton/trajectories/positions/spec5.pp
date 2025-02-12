1c55eb080c9912e48540e53d1eed9dee4063df79
Stephen Chen <stephen.chen420@gmail.com>
2025-02-12 17:09:17 -0500

    5 spec will be real beacuse im manifesting it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/trajectories/positions/spec5.pp
@@ -0,0 +1,362 @@
+{
+  "startPoint": {
+  "x": 8,
+  "y": 65,
+  "heading": "linear",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 0
+},
+"lines": [
+  {
+    "endPoint": {
+    "x": 42,
+    "y": 72,
+    "heading": "linear",
+    "startDeg": 0,
+    "endDeg": 0,
+    "name": "scorePreload",
+    "index": 1
+  },
+  "controlPoints": [
+  {
+  "x": 25,
+  "y": 62,
+  "name": "scorePreloadFirstControl",
+  "index": 2
+  },
+  {
+  "x": 25,
+  "y": 75,
+  "name": "scorePreloadSecondControl",
+  "index": 3
+  }
+],
+"color": "#6B9AD7"
+},
+{
+  "endPoint": {
+  "x": 55,
+  "y": 28,
+  "heading": "linear",
+  "reverse": false,
+  "name": "setupTop",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 4
+},
+"controlPoints": [
+  {
+    "x": 20,
+    "y": 10.5,
+    "name": "setupTopSecondControl",
+    "index": 5
+  },
+  {
+    "x": 63,
+    "y": 51.4,
+    "name": "setupTopThirdControl",
+    "index": 6
+  }
+],
+"color": "#C97AD7"
+},
+{
+  "endPoint": {
+  "x": 15,
+  "y": 28,
+  "heading": "linear",
+  "reverse": false,
+  "name": "pushTop",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 7
+},
+"controlPoints": [],
+"color": "#987A9B"
+},
+{
+  "endPoint": {
+  "x": 55,
+  "y": 15,
+  "heading": "linear",
+  "reverse": false,
+  "name": "setupMid",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 8
+},
+"controlPoints": [
+  {
+    "x": 63,
+    "y": 34,
+    "name": "setupMidControl",
+    "index": 9
+  }
+],
+"color": "#9AA56D"
+},
+{
+  "endPoint": {
+  "x": 15,
+  "y": 15,
+  "heading": "linear",
+  "reverse": false,
+  "name": "pushMid",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 10
+},
+"controlPoints": [],
+"color": "#6DC56B"
+},
+{
+  "endPoint": {
+  "x": 55,
+  "y": 13.5,
+  "heading": "linear",
+  "reverse": false,
+  "name": "setupBottom",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 11
+},
+"controlPoints": [
+  {
+    "x": 63,
+    "y": 22,
+    "name": "setupBottomControl",
+    "index": 12
+  }
+],
+"color": "#7BB679"
+},
+{
+  "endPoint": {
+  "x": 15,
+  "y": 13.5,
+  "heading": "linear",
+  "reverse": false,
+  "name": "pushBottom",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 13
+},
+"controlPoints": [],
+"color": "#85C966"
+},
+{
+  "endPoint": {
+  "x": 9,
+  "y": 34,
+  "heading": "linear",
+  "reverse": false,
+  "name": "intakeSecond",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 14
+},
+"controlPoints": [
+  {
+    "x": 20,
+    "y": 37,
+    "name": "intakeSecondControl",
+    "index": 15
+  }
+],
+"color": "#56765A"
+},
+{
+  "endPoint": {
+  "x": 42,
+  "y": 71,
+  "heading": "linear",
+  "reverse": false,
+  "startDeg": 0,
+  "endDeg": 0,
+  "name": "scoreSecond",
+  "index": 16
+},
+"controlPoints": [
+  {
+    "x": 20,
+    "y": 67,
+    "name": "scoreSecondControl",
+    "index": 17
+  }
+],
+"color": "#95886D"
+},
+{
+  "endPoint": {
+  "x": 9,
+  "y": 34,
+  "heading": "linear",
+  "reverse": false,
+  "name": "intakeThird",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 18
+},
+"controlPoints": [
+  {
+    "x": 15,
+    "y": 67,
+    "name": "intakeThirdFirstControl",
+    "index": 19
+  },
+  {
+    "x": 37,
+    "y": 30,
+    "name": "intakeThirdSecondControl",
+    "index": 20
+  }
+],
+"color": "#DA67A9"
+},
+{
+  "endPoint": {
+  "x": 42,
+  "y": 70,
+  "heading": "linear",
+  "reverse": false,
+  "name": "scoreThird",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 21
+},
+"controlPoints": [
+  {
+    "x": 20,
+    "y": 65,
+    "name": "scoreThirdControl",
+    "index": 22
+  }
+],
+"color": "#A65596"
+},
+{
+  "endPoint": {
+  "x": 9,
+  "y": 34,
+  "heading": "linear",
+  "reverse": false,
+  "name": "intakeFourth",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 23
+},
+"controlPoints": [
+  {
+    "x": 15,
+    "y": 67,
+    "name": "intakeFourthFirstControl",
+    "index": 24
+  },
+  {
+    "x": 37,
+    "y": 30,
+    "name": "intakeFourthSecondControl",
+    "index": 25
+  }
+],
+"color": "#88C58B"
+},
+{
+  "endPoint": {
+  "x": 42,
+  "y": 69,
+  "heading": "linear",
+  "reverse": false,
+  "name": "scoreFourth",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 26
+},
+"controlPoints": [
+  {
+    "x": 20,
+    "y": 63,
+    "name": "scoreFourthControl",
+    "index": 27
+  }
+],
+"color": "#D9B96B"
+},
+{
+  "endPoint": {
+  "x": 9,
+  "y": 34,
+  "heading": "linear",
+  "reverse": false,
+  "name": "intakeFifth",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 28
+},
+"controlPoints": [
+  {
+    "x": 15,
+    "y": 67,
+    "name": "intakeFifthFirstControl",
+    "index": 29
+  },
+  {
+    "x": 37,
+    "y": 30,
+    "name": "intakeFifthSecondControl",
+    "index": 30
+  }
+],
+"color": "#D5568A"
+},
+{
+  "endPoint": {
+  "x": 42,
+  "y": 68,
+  "heading": "linear",
+  "reverse": false,
+  "name": "scoreFifth",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 31
+},
+"controlPoints": [
+  {
+    "x": 20,
+    "y": 61,
+    "name": "scoreFifthControl",
+    "index": 32
+  }
+],
+"color": "#B677D7"
+},
+{
+  "endPoint": {
+  "x": 9,
+  "y": 35,
+  "heading": "linear",
+  "reverse": false,
+  "name": "park",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 33
+},
+"controlPoints": [
+  {
+    "x": 15,
+    "y": 67,
+    "name": "parkControl",
+    "index": 34
+  },
+  {
+    "x": 37,
+    "y": 30,
+    "name": "parkSecondControl",
+    "index": 35
+  }
+],
+"color": "#D8B67C"
+}
+]
+}
