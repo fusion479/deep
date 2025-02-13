e1b945bf415a83ddcdc26fda87ffe39dfef11b25
Bi1ku <2008owenshi@gmail.com>
2025-02-12 23:11:30 -0500

    wire test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/WireTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/WireTest.java
@@ -1,25 +1,30 @@
 package org.firstinspires.ftc.teamcode.opmodes.test;
 
 import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.commands.wire.WireZero;
-import org.firstinspires.ftc.teamcode.subsystems.TestWire;
 import org.firstinspires.ftc.teamcode.utils.commands.OpModeCore;
 
 @TeleOp(name = "Wire Test")
 public class WireTest extends OpModeCore {
-    private TestWire testWire;
+    private Servo wire;
     private GamepadEx gamepad;
 
     @Override
     public void initialize() {
+        super.initialize();
+
         // one of these is a cr servo as well - hopefully everything else will work except that one
-        this.gamepad = new GamepadEx(super.gamepad1);
-        testWire = new TestWire(super.hardwareMap);
-        this.gamepad.getGamepadButton(GamepadKeys.Button.A).whenPressed(new WireZero(this.testWire));
+        this.wire = super.hardwareMap.get(Servo.class, "wire");
+        this.gamepad = new GamepadEx(this.gamepad1);
+        this.gamepad.getGamepadButton(GamepadKeys.Button.A).whenPressed(new InstantCommand(() -> this.wire.setPosition(1)));
+        this.gamepad.getGamepadButton(GamepadKeys.Button.B).whenPressed(new InstantCommand(() -> this.wire.setPosition(0)));
+
+        this.wire.setPosition(1);
     }
 
     @Override
