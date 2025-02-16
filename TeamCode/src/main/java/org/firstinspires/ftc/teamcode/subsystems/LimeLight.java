9eacb33dad078c398601ee0b078e961892a02efb
Stephen Chen <stephen.chen420@gmail.com>
2025-02-16 18:30:06 -0500

    python file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimeLight.java
@@ -1,2 +1,55 @@
-package org.firstinspires.ftc.teamcode.subsystems;public class LimeLight {
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class LimeLight extends SubsystemBase {
+    private Limelight3A limelight;
+    private double ta, tx, ty, tangle;
+
+    public static int PIPELINE = 7; //placeholder
+
+    public LimeLight(final HardwareMap hwMap){
+        this.limelight = hwMap.get(Limelight3A.class, "limeight");
+
+        limelight.setPollRateHz(100);
+
+        limelight.pipelineSwitch(PIPELINE);
+        limelight.start();
+    }
+
+    public void update() {
+        LLResult result = limelight.getLatestResult();
+        if (result != null) {
+            ta = result.getTa();
+            tx = result.getTx();
+            ty = result.getTy();
+            tangle = result.getPythonOutput()[3];
+        }
+    }
+
+    public void stop() {
+        limelight.stop();
+    }
+
+    public void setPipeline(int pipeline) {
+        limelight.pipelineSwitch(pipeline);
+    }
+
+    public double getTa() {
+        return ta;
+    }
+    public double getTx() {
+        return tx;
+    }
+
+    public double getTy() {
+        return ty;
+    }
+
+    public double getTangle() {
+        return tangle;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/LimeLightConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/LimeLightConstants.java
@@ -1,2 +1,19 @@
-package org.firstinspires.ftc.teamcode.utils;public class LimeLightConstants {
+package org.firstinspires.ftc.teamcode.utils;
+
+public class LimeLightConstants {
+    public static double LIME_LIGHT_MOUNT_ANGLE = Math.toRadians(0); // a1
+    public static double LIME_LIGHT_LENS_HEIGHT_INCHES = 0; // h1
+    public static double SAMPLE_HEIGHT_INCHES = 0; // h2
+
+    public static double calcYDistance(double ty) { // a2
+        double angleToSampleRadians = LIME_LIGHT_MOUNT_ANGLE - Math.toRadians(ty);
+
+        return Math.abs((SAMPLE_HEIGHT_INCHES - LIME_LIGHT_LENS_HEIGHT_INCHES) / Math.tan(angleToSampleRadians) + 1.5);
+    }
+
+    public static double calcXDistance(double tx, double ty) { // a2
+        double angleToSampleRadians = Math.toRadians(tx);
+
+        return calcYDistance(ty) * Math.tan(angleToSampleRadians);
+    }
 }
