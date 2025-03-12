12d0554e274009515ed19be38c3e80cb1b897672
Stephen Chen <stephen.chen420@gmail.com>
2025-03-12 15:59:30 -0400

    spec 5 sweep path

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auton/trajectories/positions/spec5sweep.pp
@@ -0,0 +1,263 @@
+{
+  "startPoint": {
+  "x": 8,
+  "y": 65,
+  "heading": "linear",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 0
+},
+"lines": [
+  {
+    "endPoint": {
+    "x": 35.5,
+    "y": 72,
+    "heading": "linear",
+    "startDeg": 0,
+    "endDeg": 0,
+    "name": "scorePreload",
+    "index": 1
+  },
+  "controlPoints": [
+  {
+  "x": 25,
+  "y": 62,
+  "name": "scorePreloadFirstControl",
+  "index": 2
+  },
+  {
+  "x": 18,
+  "y": 75,
+  "name": "scorePreloadSecondControl",
+  "index": 3
+  }
+],
+"color": "#6B9AD7"
+},
+{
+  "endPoint": {
+  "x": 36,
+  "y": 36,
+  "heading": "linear",
+  "reverse": false,
+  "name": "setupTop",
+  "startDeg": 0,
+  "endDeg": 320,
+  "index": 4,
+  "degrees": 0
+},
+"controlPoints": [
+  {
+    "x": 20,
+    "y": 40,
+    "name": "setupTopSecondControl",
+    "index": 5
+  }
+],
+"color": "#C97AD7"
+},
+{
+  "endPoint": {
+  "x": 25,
+  "y": 36,
+  "heading": "linear",
+  "reverse": false,
+  "name": "sweepTop",
+  "startDeg": 320,
+  "endDeg": 225,
+  "index": 6
+},
+"controlPoints": [],
+"color": "#B7DCA5"
+},
+{
+  "endPoint": {
+  "x": 36,
+  "y": 24,
+  "heading": "linear",
+  "reverse": false,
+  "name": "setupMiddle",
+  "startDeg": 225,
+  "endDeg": 320,
+  "index": 7
+},
+"controlPoints": [],
+"color": "#D9C968"
+},
+{
+  "endPoint": {
+  "x": 25,
+  "y": 24,
+  "heading": "linear",
+  "reverse": false,
+  "name": "sweepMiddle",
+  "startDeg": 320,
+  "endDeg": 225,
+  "index": 8
+},
+"controlPoints": [],
+"color": "#8CAC79"
+},
+{
+  "endPoint": {
+  "x": 36,
+  "y": 14,
+  "heading": "linear",
+  "reverse": false,
+  "name": "setupBottom",
+  "startDeg": 225,
+  "endDeg": 320,
+  "index": 9
+},
+"controlPoints": [],
+"color": "#A6ACDB"
+},
+{
+  "endPoint": {
+  "x": 25,
+  "y": 14,
+  "heading": "linear",
+  "reverse": false,
+  "name": "sweepBottom",
+  "startDeg": 320,
+  "endDeg": 225,
+  "index": 10
+},
+"controlPoints": [],
+"color": "#B7B88C"
+},
+{
+  "endPoint": {
+  "x": 16,
+  "y": 14,
+  "heading": "linear",
+  "reverse": false,
+  "name": "intakeSecond",
+  "startDeg": 225,
+  "endDeg": 0,
+  "index": 11
+},
+"controlPoints": [],
+"color": "#B7B88C"
+},
+{
+  "endPoint": {
+  "x": 31,
+  "y": 67,
+  "heading": "linear",
+  "reverse": false,
+  "name": "scoreSecond",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 12
+},
+"controlPoints": [
+  {
+    "x": 20,
+    "y": 70,
+    "name": "scoreSecondControl",
+    "index": 13
+  }
+],
+"color": "#B755BB"
+},
+{
+  "endPoint": {
+  "x": 16,
+  "y": 39,
+  "heading": "linear",
+  "reverse": false,
+  "name": "intakeThird",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 14
+},
+"controlPoints": [],
+"color": "#8A796B"
+},
+{
+  "endPoint": {
+  "x": 34,
+  "y": 70,
+  "heading": "linear",
+  "reverse": false,
+  "name": "scoreThird",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 15
+},
+"controlPoints": [],
+"color": "#5B7D9C"
+},
+{
+  "endPoint": {
+  "x": 16,
+  "y": 39,
+  "heading": "linear",
+  "reverse": false,
+  "name": "intakeFourth",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 16
+},
+"controlPoints": [],
+"color": "#6AD79B"
+},
+{
+  "endPoint": {
+  "x": 34.5,
+  "y": 69,
+  "heading": "linear",
+  "reverse": false,
+  "name": "scoreFourth",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 17
+},
+"controlPoints": [],
+"color": "#98D5B5"
+},
+{
+  "endPoint": {
+  "x": 16,
+  "y": 39,
+  "heading": "linear",
+  "reverse": false,
+  "name": "intakeFifth",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 18
+},
+"controlPoints": [],
+"color": "#D9998A"
+},
+{
+  "endPoint": {
+  "x": 35,
+  "y": 68,
+  "heading": "linear",
+  "reverse": false,
+  "name": "scoreFifth",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 19
+},
+"controlPoints": [],
+"color": "#97BA65"
+},
+{
+  "endPoint": {
+  "x": 9,
+  "y": 34,
+  "heading": "linear",
+  "reverse": false,
+  "name": "park",
+  "startDeg": 0,
+  "endDeg": 0,
+  "index": 20
+},
+"controlPoints": [],
+"color": "#5BB6B9"
+}
+]
+}
